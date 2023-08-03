089df869b393e406c73c1ad73edfb0c051340cb4
Joseph Gardner <joshgd626@gmail.com>
2023-08-02 20:48:19 -0500

    gitigmore

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
-    $mail_sender_email = "purpleis@purpleishtech.com"; //sender
-    $mail_sender_password = "e6&j2etH"; //sender
-    $mail_sender_name = "Website Form";
+    $mail_sender_email = ""; //sender
+    $mail_sender_password = ""; //sender
+    $mail_sender_name = "Purpleish Tech LLC";
 ?>
